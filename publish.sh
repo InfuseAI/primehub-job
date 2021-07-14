ff63dde242ca75ee0498e8b0d5e200cd6d77cf6e
ericy <ericy@infuseai.io>
2021-07-14 10:54:35 +0800

    add comment
    Signed-off-by: ericy <ericy@infuseai.io>

--- publish.sh
+++ publish.sh
@@ -1,12 +1,11 @@
+# pypi: primehub-job
 python3 setup.py sdist bdist_wheel
 python3 -m twine upload dist/*
 
+# npm: jupyterlab-primehub
 cd jupyterlab_primehub
-conda activate jupyterlab-ext
-
 yarn publish --access public
 
+# pypi: jupyterlab-primehub
 python3 setup.py sdist bdist_wheel
 python3 -m twine upload dist/*
-
-conda deactivate

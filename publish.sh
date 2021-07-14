cc7578c13d7e5a0f9d3a346c74bc1629e4af0e46
ericy <ericy@infuseai.io>
2021-07-14 09:47:47 +0800

    bump version
    Signed-off-by: ericy <ericy@infuseai.io>

--- publish.sh
+++ publish.sh
@@ -2,7 +2,6 @@ python3 setup.py sdist bdist_wheel
 python3 -m twine upload dist/*
 
 cd jupyterlab_primehub
-source /Users/jackpan/miniconda3/etc/profile.d/conda.sh
 conda activate jupyterlab-ext
 
 yarn publish --access public
